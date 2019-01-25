2b97d11d7ffe7aad3cb962aff0daa4a4bfcd19c5
wa81-15 <khemirifatima@gmail.com>
2019-01-25 12:11:42 +0100

    Part 2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -34,7 +40,46 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/{id}", name="blog_show")
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function create(Article $article = null, Request $request, ObjectManager $manager) {
+
+        if(!$article) {
+            $article = new Article();
+        }
+
+        //$form = $this->createFormBuilder($article)
+        //            ->add('title')
+        //            ->add('content')
+        //            ->add('image')
+        //            ->getForm();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        //dump($article)
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if(!$article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show", requirements={"id":"\d+"})
      */
     public function show(Article $article)
     {
